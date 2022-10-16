bc32a129872843fc1f3502c788477654552dafd9
Pumipat <88040178+o0SoloWolf0o@users.noreply.github.com>
2022-10-16 15:27:09 +0700

    master

--- CSS222/java_main/src/Animals.java
+++ CSS222/java_main/src/Animals.java
@@ -2,26 +2,47 @@ class Animal {
     public void speak() {
         System.out.println("Animal Speak !");
     }
+}
 
-    class Dog extends Animal {
-        public void speak() {
-            bark();
-        }
-
-        public void bark() {
-            System.out.println("Woof !");
-        }
+class Dog extends Animal {
+    public void speak() {
+        bark();
     }
 
-    class Seal extends Animal {
-        public void bark() {
-            System.out.println("Arf !");
-        }
+    public void bark() {
+        System.out.println("Woof !");
+    }
+}
+
+class Seal extends Animal {
+    public void speak() {
+        bark();
     }
 
-    class Bird extends Animal {
-    public void speak () { 
+    public void bark() {
+        System.out.println("Arf !");
+    }
+}
+
+class Bird extends Animal {
+    public void speak() {
+        bark();
+    }
+
+    public void bark() {
         System.out.println("Tweet !");
-        }
+    }
+}
+
+class useanimal {
+    public static void main(String[] args) {
+        Animal a = new Animal();
+        Animal d = new Dog();
+        Animal s = new Seal();
+        Animal b = new Bird();
+        a.speak();
+        d.speak();
+        s.speak();
+        b.speak();
     }
 }
--- CSS222/java_main/src/ExampleAbstraction.java
+++ CSS222/java_main/src/ExampleAbstraction.java
@@ -1,20 +1,20 @@
-abstract class Animal{
-    public abstract void animalSound();
-    public void sleep(){
-        System.out.println("Zzz");
-    }
-}
+// abstract class Animal{
+//     public abstract void animalSound();
+//     public void sleep(){
+//         System.out.println("Zzz");
+//     }
+// }
 
-class Pig extends Animal {
-    public void animalSound(){
-        System.out.println("The pig says: wee wee");
-    }
-}
+// class Pig extends Animal {
+//     public void animalSound(){
+//         System.out.println("The pig says: wee wee");
+//     }
+// }
 
-class ExampleAbstract {
-    public static void main(String[] args) {
-        Pig myPig = new Pig();
-        myPig.animalSound();
-        myPig.sleep();
-    }
-}
+// class ExampleAbstract {
+//     public static void main(String[] args) {
+//         Pig myPig = new Pig();
+//         myPig.animalSound();
+//         myPig.sleep();
+//     }
+// }
